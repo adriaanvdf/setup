631f24743638615445c1a6fdaf026adba1e99f75
Adriaan van der Feltz <29247831+adriaanvdf@users.noreply.github.com>
2022-07-21 18:22:44 +0200

    Create setup.sh
    Initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install packages
+brew install iperf3
+
+# References
+# https://gist.github.com/bradp/bea76b16d3325f5c47d4
