5156eb1560ae358365d4c217459fe5a9eb6eb234
Adriaan van der Feltz <29247831+adriaanvdf@users.noreply.github.com>
2022-07-27 20:08:13 +0200

    Update setup.sh
    add install for go

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 # Install packages
 brew install iperf3
+brew install go
 
 # References
 # https://gist.github.com/bradp/bea76b16d3325f5c47d4
