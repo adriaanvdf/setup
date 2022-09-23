36577e8b4226786e90b4d71ceb2d31d9043d10c6
adriaanvdf <github@vdfeltz.com>
2022-09-23 17:51:45 +0200

    rename file and create loop to install both packages and apps

---
+++ mac.sh
@@ -0,0 +1,78 @@
+#!/bin/bash -e -x
+
+# Install Homebrew
+if test ! $(which brew); then
+    echo "Installing Hombrew"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# else
+#     echo "Updating Homebrew"
+#     brew update
+fi	
+
+
+packages=(
+	go
+	iperf3
+	kubectl
+	kubectx
+	minikube
+	nmap
+	pv
+	ssh
+	wget
+	xzcat
+	)
+
+applications=(
+	raspberry-pi-imager
+	sublime-merge
+	sublime-text
+	)
+
+software=(
+	"${packages[@]}"
+	"${applications[@]}"
+	)
+
+# # Install packages
+# for package in "${packages[@]}"; do
+# 	if test ! $(which $package); then
+# 	    echo "Installing $package"
+# 	    brew install $package
+# 	else
+# 	    echo "$package already installed"
+# 	#     echo "Updating $package"
+# 	#     brew update <>
+# 	fi
+# done
+
+# # Install packages
+# for app in "${applications[@]}"; do
+# 	if test ! $(which $app); then
+# 	    echo "Installing $app"
+# 	    brew install --cask $
+# 	    brew cask install --appdir="/Applications" $app
+
+# 	# else
+# 	#     echo "Updating $app"
+# 	#     brew update <>
+# 	fi
+# done
+
+# Install software
+for i in "${software[@]}"; do
+	if brew list $i &>/dev/null; then
+		echo "$i already installed"
+		# echo "Updating $i"
+		# brew update <>
+	else
+		echo "Installing $i"
+	    brew install $i
+
+	fi
+done
+
+brew cleanup
+
+# Create symlink shortcuts
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
--- setup.sh
+++
@@ -1,9 +0,0 @@
-# Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install packages
-brew install iperf3
-brew install go
-
-# References
-# https://gist.github.com/bradp/bea76b16d3325f5c47d4
